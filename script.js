32dc6a0ade57719f86e3e629581db8df746ef400
Oliver Lind Kristiansen <79264222+olindkri@users.noreply.github.com>
2023-09-15 15:38:36 +0200

    Created script.js file.

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleText() {
+  var text = document.getElementById("demo");
+  if (text.style.display === "none") {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
